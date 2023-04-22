9017f91cd657e84191aa6e40976398192f029818
张雷 <3537168410@qq.com>
2023-04-23 00:20:13 +0800

    first commit

---
+++ 服务端/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
+const app = express();
+json = express.json({type:'*/json'});
+app.use(cors())
+app.use(json);
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.post('/',(req,res)=>{
+    let name = +new Date();
+    fs.writeFile('./../文章/'+name+'.json',JSON.stringify(req.body),(err,data)=>{
+        if(err) res.send('error');
+        res.send('ok');
+    });
+})
+app.listen(3000,()=>{
+    console.log('服务端已经运行...');
+})
