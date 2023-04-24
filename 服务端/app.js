2d6de3835728969b63e56339260f6f4debc81a0c
张雷 <3537168410@qq.com>
2023-04-24 18:03:27 +0800

    新增预览功能,和查看全部功能,增加提示

--- 服务端/app.js
+++ 服务端/app.js
@@ -10,11 +10,29 @@ app.use(json);
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.post('/',(req,res)=>{
-    let name = +new Date();
-    fs.writeFile('./文章/'+name+'.json',JSON.stringify(req.body),(err,data)=>{
+    // let name = +new Date();
+    fs.writeFile('./文章/'+req.body.title+'.json',JSON.stringify(req.body.layout),(err,data)=>{
         if(err) res.send({type:'error'});
         res.send({type:'ok'});
     });
+})
+app.get('/',(req,res)=>{
+    const articleArr = [];
+    fs.readdir('./文章/',(err,files)=>{
+        files.forEach(filename=>{
+            articleArr.push(filename);
+        })
+        console.log(articleArr);
+        // res.send({data:articleArr});
+        res.end(JSON.stringify(articleArr));
+    })
+})
+app.get('/article/:articleName',(req,res)=>{
+    console.log(req.params.articleName);
+    fs.readFile('./文章/'+req.params.articleName,(err,data)=>{
+        if(err) res.end({type:'error'});
+        res.end(data);
+    })
 })
 app.listen(3000,()=>{
     console.log('服务端已经运行...');
